5770397276c78538f333bac2404388d8102bdfb2
brightzest007 <emenibright@gmail.com>
2022-12-09 08:12:41 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - Prints the alphabet.
+ *   *
+ *    * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz"
+	
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpa[i]);
+	}
+	putchar('\n');
+return (0)
+}
