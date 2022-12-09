da9371b3666c4d3a3d5073c6a180143a377ccf44
brightzest007 <emenibright@gmail.com>
2022-12-09 14:15:01 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  *  * main - Prints numbers between z to a.
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	        int j;
+		        for (j = 122; j >96; j--)
+				        {
+						                putchar(j);
+								        }
+			        putchar('\n');
+				        return (0);
+}
