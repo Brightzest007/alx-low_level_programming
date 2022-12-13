fce2aedce57ce9f90041bda377c39785029e528b
brightzest007 <emenibright@gmail.com>
2022-12-12 23:07:16 -0800

    Prints _putchar using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar followed by a newline
+ *
+ * Return - returns 0 (Sucess)
+ */
+
+int main(void)
+{
+	int _putchar('_');
+	int _putchar('p');
+	int _putchar('u');
+	int _putchar('t');
+	int _putchar('c');
+	int _putchar('h');
+	int _putchar('a');
+	int _putchar('r');
+	int _putchar('\n');
+
+	return (0);
+}
