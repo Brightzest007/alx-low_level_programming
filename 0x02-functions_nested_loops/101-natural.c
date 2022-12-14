b0629a21ea2239181e11389b1f6f69918ad646f0
brightzest007 <emenibright@gmail.com>
2022-12-13 21:23:40 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,18 +5,16 @@
  *   * multiplies of 3 or 5
  *    *
  *     * Return: Always 0.
- */
+ *      */
 int main(void)
 {
-	int a, b;
+		int a, b;
 
-	for (a = 1; a < 1024; a++)
-	{
-		if ((a % 3) == 0 || (a % 5) == 0)
-		{
-			b += a;
-		}
-		printf("%d\n", b);
-		return (0);
-	}
+			for (a = 1; a < 1024; a++)
+					{
+								if ((a % 3) == 0 || (a % 5) == 0)
+												b += a;
+									}
+				printf("%d\n", b);
+					return (0);
 }
