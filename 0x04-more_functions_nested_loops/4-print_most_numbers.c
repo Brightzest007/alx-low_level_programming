c4b3d23f6e5ee8f6f9fe8b41d6b3195b0c480249
brightzest007 <emenibright@gmail.com>
2022-12-16 11:24:06 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,11 @@ void print_most_numbers(void)
 {
 	int j;
 
-	for (ch = 48; ch < 58; ch++)
+	for (j = 48; j < 58; j++)
 	{
-		if (ch != 50 && ch != 52)
+		if (j != 50 && j != 52)
 		{	
-			_putchar(ch);
+			_putchar(j);
 		}
 	}
 	_putchar('\n');
